68088c942144ddc8f96fe3357fbb566f8e721b46
leenahmad <leenkaraja24@gmail.com>
2021-09-21 15:03:11 +0300

    lab7 server

--- server.js
+++ server.js
@@ -1,51 +1,50 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 
 const server = express();
 
-const searchQuery = require('./data/weather.json');
-const { request, response } = require('express');
+const weather1 = require("./data/weather.json");
+const { request, response } = require("express");
 
 const PORT = process.env.PORT;
 
 server.use(cors());
 
-
 class Date {
-    constructor(date, description){
-        this.data = date,
-        this.description = description
-    }
+  constructor(date, description) {
+    (this.data = date), (this.description = description);
+  }
 }
 
-
-server.get('/',(req,res) => {
-    res.status(200).send('home router')
+server.get("/", (req, res) => {
+  res.status(200).send("home router");
 })
 
-server.get('/weather' , (request,response) => {
-    let q = request.query.q;
-    let location = searchQuery.find(location =>location.city_name==q);
-    let weather = [];
-    location.data.forEach(i =>{
-        weather.push(
-            new Date(i.datetime, `with ${i.weather.description}`)
-        )
-    })
-    response.send(weather);
+server.get("/weather", (request, response) => {
+  let searchQuert = request.query.searchQuert;
+
+  
+  let data1 = weather1.find((element) => {
+    if (element.city_name === searchQuert) {
+      return element;
+    }
+  })
+
+  let array = data1.data.map((value) => {
+    return new Date(value.datetime, value.weather.description);
+  })
+response.send(array)
 })
 
-server.get('*' , (req,res) => {
-    res.status(404).send('route is not found')
-})
+server.get("*", (req, res) => {
+  res.status(404).send("route is not found");
+});
 
 server.listen(PORT, () => {
-    console.log(`LISTENING ON PORT ${PORT}`)
-})
-
-
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
